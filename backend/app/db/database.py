f0856c773a9f2276f9e430b7b7e70dac26a8bdf8
alokrj01 <ranjanalok2468@gmail.com>
2026-06-01 14:32:09 +0530

    feat: add initial database schema with alembic

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-#DATABASE_URL = "postgresql://postgres:P9e81VYoaeQHxEQi@db.mskmbvoskmkumjpwxicl.supabase.co:5432/postgres"
-
 engine = create_engine(
   settings.DATABASE_URL,
   pool_pre_ping=True
